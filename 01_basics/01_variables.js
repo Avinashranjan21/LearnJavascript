43eb2fe61e2b6bc4322f4bfe9aa2b8213bdfb712
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-10-07 00:59:41 +0530

    Datatype and variables intro completed

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,3 +1,7 @@
-const firstname = "Avinash Pandey"
+const firstname = "Avinash";
+let lastName = "Pandey";
+age = 32;
+emailId = "Avinash.Ranjan21@Gmail.com";
 
 console.log("My name is " + firstname);
+console.table([firstname, lastName, age, emailId]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "Avinash"
+let age = 32
+let isAdult = true
+
+// Type of variable in JS
+/*
+String => 
+symbol => This is used for uniqueness in JS
+Undefined => Means value is not yet assigned
+null => Standlaone value and this is treated as object in JS unlike other languages
+
+Other data type is as other languages
+*/
+
+console.log("The type of null is " + typeof null)
+console.log("The type of undefined is " + typeof undefined)
