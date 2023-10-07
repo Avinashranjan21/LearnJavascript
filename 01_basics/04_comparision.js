1a71e28021526b973bfd1909b3d5b42c3189c400
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-10-07 18:18:17 +0530

    comparision operators complted

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,8 @@
+console.log(null == 0) // false
+console.log(null >= 0) // true
+console.log(null <= 0) // true
+
+// === matches the values along with data types
+console.log("-------- === cases ---------");
+console.log("2" === 2);
+console.log("2" === "2");
