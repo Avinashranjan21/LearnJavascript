72fcbf83d92bdc7ca9103b95c81922056b4012f2
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-10-08 19:47:53 +0530

    Data type summary added

---
+++ 01_basics/05_datatypeSummary.js
@@ -0,0 +1,9 @@
+/* 
+Javascript is dynamically typed language.
+
+#1 Primitive -> They are call by value and of 7 types which is mentioned below
+String, Number,Boolean, null, undefined, Symbol, BigInt
+
+#2: Reference (Non-Primitive) 
+Arrays, Objects, Functions
+*/
