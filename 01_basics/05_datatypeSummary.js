0446419561316614582e003aa4fdf0505e9af2a6
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-10-11 19:34:12 +0530

    stack ans heap completed

--- 01_basics/05_datatypeSummary.js
+++ 01_basics/05_datatypeSummary.js
@@ -7,3 +7,31 @@ String, Number,Boolean, null, undefined, Symbol, BigInt
 #2: Reference (Non-Primitive) 
 Arrays, Objects, Functions
 */
+
+
+// Stack and Heaps
+
+// All the primitive data type value is kept at stack and non primitive is stored in heaps.
+// Primitive is call by value means when another primitive is refered to this then value is sent not the reference
+
+let myname = "Avinash Pandey"
+let myAnotherName = "Avinash Ranjan"
+
+console.log("myname value is " + myname);
+console.log("myAnotherName value is " + myAnotherName);
+myname = "Little baua"
+console.log("myname value is " + myname);
+console.log("myAnotherName value is " + myAnotherName);
+
+
+let userOne = {
+    name : "Avinash Ranjan",
+    age : 30
+}
+
+let userTwo = userOne
+
+console.log("The usertwo name is " + userTwo.name);
+userOne.name = "Avinash Pandey"
+// As this is stored in heaps so the reference is transferred.
+console.log("The usertwo name is " + userTwo.name);
