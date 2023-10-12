83d7de58e646af6f65e84fed052b36fc213016c4
Avinash Pandey <Avinash.Ranjan21@gmail.com>
2023-10-12 20:19:58 +0530

    String practice pushed

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,12 @@
+const name = "Avinash Pandey"
+const repoCount = 120
+
+//console.log(name + repoCount + " value"); Not recommended
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
+ 
+const fullName = "Avinash Pandey"
+
+console.log(`Full name is upper case is ${fullName.toUpperCase()}`);
+console.log(`Full name is lower case is ${fullName.toLowerCase()}`);
+console.log(`Full name length is ${fullName.length}`);
+console.log(`Full name trim for first name is ${fullName.substring(0, fullName.indexOf(" "))}`);
